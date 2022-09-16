45cef513d7ad9cc44f103064d33f9d16a39897ed
Lauren Hodge <scimminni@hotmail.com>
2022-09-16 09:31:39 +0000

    Add crispy forms no submit comment

--- blango/settings.py
+++ blango/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +61,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blango.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
